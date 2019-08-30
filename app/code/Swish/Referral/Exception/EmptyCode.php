ca5fb6984cc473ac7523654d8f776787b7a41eae
kharidas <kharidas.chebotaryov@gmail.com>
2019-08-31 01:27:42 +0300

    no customer code exception

--- app/code/Swish/Referral/Exception/EmptyCode.php
+++ app/code/Swish/Referral/Exception/EmptyCode.php
@@ -8,8 +8,44 @@
 
 namespace Swish\Referral\Exception;
 
+use Exception;
+use Magento\Framework\Phrase;
 
-class EmptyCode
+class EmptyCode extends Exception
 {
+    /**
+     * @var Phrase
+     */
+    protected $phrase;
 
+    /**
+     * @param Phrase $phrase
+     * @param Exception $cause
+     * @param int $code
+     */
+    public function __construct(Phrase $phrase, Exception $cause = null, $code = 0)
+    {
+        $this->phrase = $phrase;
+        parent::__construct($phrase->render(), (int)$code, $cause);
+    }
+
+    /**
+     * Get the un-processed message, without the parameters filled in
+     *
+     * @return string
+     */
+    public function getRawMessage()
+    {
+        return $this->phrase->getText();
+    }
+
+    /**
+     * Get parameters, corresponding to placeholders in raw exception message
+     *
+     * @return array
+     */
+    public function getParameters()
+    {
+        return $this->phrase->getArguments();
+    }
 }
