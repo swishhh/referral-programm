2842587398a0ce5c7ec823dbfcef9edf8619f0bb
kharidas <kharidas.chebotaryov@gmail.com>
2019-08-31 02:09:40 +0300

    Setup

---
+++ app/code/Swish/Referral/Model/ReferralCode.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kharidas
+ * Date: 31.08.19
+ * Time: 01:29
+ */
+
+namespace Swish\Referral\Model;
+
+use Swish\Referral\Api\ReferralCodeInterface;
+
+class ReferralCode implements ReferralCodeInterface
+{
+    /**
+     * @inheritDoc
+     */
+    public function get()
+    {
+        // TODO: Implement get() method.
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function set()
+    {
+        // TODO: Implement set() method.
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function generate()
+    {
+        // TODO: Implement generate() method.
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function remove()
+    {
+        // TODO: Implement remove() method.
+    }
+}
