ef4f3b88a862aef449c16cca0c78c98da01d9dd2
kharidas <kharidas.chebotaryov@gmail.com>
2019-08-31 03:06:34 +0300

    Customer account link & content

---
+++ app/code/Swish/Referral/Block/Customer/Account/Content.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kharidas
+ * Date: 31.08.19
+ * Time: 03:03
+ */
+
+namespace Swish\Referral\Block\Customer\Account;
+
+
+class Content
+{
+
+}
---
+++ app/code/Swish/Referral/Controller/Customer/Index.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kharidas
+ * Date: 31.08.19
+ * Time: 03:01
+ */
+
+namespace Swish\Referral\Controller\Customer;
+
+
+class Index
+{
+
+}
